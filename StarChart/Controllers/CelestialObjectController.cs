30737d60b7e7933b9a22315c7e40361b48ed9b54
Michal Och <michal.och@pl.abb.com>
2020-08-04 15:51:31 +0200

    Create the Create Method

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -59,6 +60,18 @@ namespace StarChart.Controllers
                 celestialObject.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == celestialObject.Id).ToList();
             }
             return Ok(celestialObjects);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CelestialObject celestialObject)
+        {
+            _context.CelestialObjects.Add(celestialObject);
+            _context.SaveChanges();
+            return CreatedAtRoute("GetById", new
+            {
+                id = celestialObject.Id,
+                celestialObject
+            });
         }
     }
 }
